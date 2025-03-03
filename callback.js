1a3bcb30e5b612bd05eda5a9496062da40fcf760
lucidshaya <ohine.ivori@gmail.com>
2025-03-03 22:21:47 +0100

    promise 1

---
+++ callback.js
@@ -0,0 +1,9 @@
+// synchronous code
+
+console.log(1);
+setTimeout(() => console.log(2), 2000);
+console.log(3);
+
+
+// An object that may return a value is called a promise
+// 3 states 1 pending 2 fufiled 3 rejected 
