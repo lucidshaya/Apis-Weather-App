c77d2819a2ef8bc0eeeb563f83a49f1051857107
lucidshaya <ohine.ivori@gmail.com>
2025-03-05 18:29:38 +0100

    promise 2

--- callback.js
+++ callback.js
@@ -1,36 +1,25 @@
-// async
+// Promise 
 
-const showMessage = (message) => { // Function to display a message
-    console.log(message); // Log the message to the console
-};
+const promise = new Promise((resolve, reject) => {
 
-const firstMessage = (callback) => { // Function to show the first message and then call a callback
-    setTimeout(() => { // Delay execution by 2 seconds
-        showMessage('Hello'); // Show 'Hello' message
-        callback(); // Call the callback function
-    }, 2000);
-};
+     const randomNumber = Math.floor(Math.random() * 10 );
 
-const secondMessage = () => { // Function to show the second message
-    showMessage('world!'); // Show 'world!' message
-};
-
-firstMessage(secondMessage); // Call firstMessage and pass secondMessage as the callback
-
-
-const displayLogs = (logs) => { // Function to display logs
-    console.log(logs); // Log the logs to the console
-};
-
-const showlogs = (callback) => {
     setTimeout(() => {
-      displayLogs('user, 1 , 2 , 3 , 4 , 5')
-      callback();
-    }, 2000);
-}
+        if(randomNumber < 8) {
+            resolve("hurray")         
+        } else{
+            reject("failed")         
+        }
+    })
+});
 
-const showPasswords = (pass) => {
-    console.log(pass);
-}
 
-showlogs(showPasswords)
+
+
+promise
+.then((value) => { 
+    console.log(value);
+    
+}).catch((error) => {
+    console.log(error);
+})
