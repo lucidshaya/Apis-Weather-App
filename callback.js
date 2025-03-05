ede973cb27908bb2e4ea4091377bb840e4cfd757
lucidshaya <ohine.ivori@gmail.com>
2025-03-05 19:26:17 +0100

    async await proper

--- callback.js
+++ callback.js
@@ -1,17 +1,54 @@
-// Promise for catching errors 
+//Promises / Async Await 
 
-const promiseOne  = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('Promise One Resolve')
-    }, 2000)
-})
+const preHeatOven = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const preHeatOven = true;
+            if (preHeatOven) {
+                resolve('preheat oven 1999');
+            } else {
+                reject("failed task");
+            }
+        }, 1000);
+    });
+}
 
-const promiseTwo  = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('Promise Two Resolve')
-    }, 3000)
-})
+const addSugarChips = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const sugarChipsAdded = true;
+            if (sugarChipsAdded) {
+                resolve('sugar chips added');
+            } else {
+                reject("failed to add sugar chips");
+            }
+        }, 1000);
+    });
+}
 
-Promise.all([promiseOne, promiseTwo])
-.then((data) => console.log(data[0], data[1]))
-.catch((error) => console.log(error));
+const addMilkBiscuits = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const milkBiscuitsAdded = true;
+            if (milkBiscuitsAdded) {
+                resolve('milk biscuits added');
+            } else {
+                reject("failed to add milk biscuits");
+            }
+        }, 1000);
+    });
+}
+    
+const bakeChocolateBrownies = async () => {
+    const taskone = await preHeatOven();
+    console.log(taskone);
+
+    const tasktwo = await addSugarChips();
+    console.log(tasktwo);
+    
+    const taskThree = await addMilkBiscuits();
+    console.log(taskThree);
+    
+}
+
+bakeChocolateBrownies()
